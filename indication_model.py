4b634187b73c7cdd34f3f05d35e0fde17451812d
Keenan Berry <keenanberry22@gmail.com>
2019-08-02 14:52:35 -0500

    Add files via upload

---
+++ indication_model.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import re
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neighbors import NearestNeighbors
+
+
+def normalize_text(text):
+    for i in range(len(text)):
+        s = text[i]
+
+        if isinstance(s, str):
+            sp = re.sub('\W+', ' ', s.lower())
+            sp = re.sub(r'[0-9]+', '', sp)
+            sp = re.sub(r'  +', ' ', sp)
+            text[i] = sp.strip()
+        # remove floats/ints
+        elif isinstance(s, (int, float)):
+            text[i] = 'null' 
+
+    return text
+
+
+def process_clean_indication(clean_data):
+    clean_data['Condition'].fillna('null', inplace=True)
+    clean_data['Condition'].replace('', 'null', inplace=True)
+
+    clean_data['Indication'] = clean_data.loc[:,'Indication'].str.lower() 
+    clean_data['Condition'] = normalize_text(list(clean_data['Condition']))
+
+    return clean_data
+
+
+def process_unclean_indication(unclean_data):
+    unclean_data['Condition'] = normalize_text(list(unclean_data['Condition']))
+
+    return unclean_data
+
+
+def get_indication_neighbors(clean, unclean, neighbors):
+    clean = process_clean_indication(clean)
+    unclean = process_unclean_indication(unclean)
+
+    #encoder = preprocessing.LabelEncoder()
+    #encoder.fit(clean['Indication'])
+    #y_train = encoder.transform(clean['Indication'])
+    
+    X_train = list(clean['Condition'])
+    X_test = list(unclean['Condition'])
+
+    # build and train model
+    cv = CountVectorizer(analyzer='word', max_df = 0.40, min_df = 1, ngram_range=(1,1), stop_words = 'english')
+    train_vector = cv.fit_transform(X_train)
+    test_vector = cv.transform(X_test)
+    
+    nn = NearestNeighbors(n_neighbors=neighbors, metric='cosine', p=1)
+    nn.fit(train_vector)
+    neighborhood = nn.kneighbors(test_vector, return_distance=True)
+
+    return(neighborhood)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
-#import magic
+import os, sys
 import urllib.request
 from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
+import pandas as pd
+import operator
+from collections import Counter
 
 ALLOWED_EXTENSIONS = set(['xlsx', 'xls'])
 
@@ -11,17 +13,18 @@ def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def upload_form():
-	return render_template('upload.html')
+def index():
+	return render_template('index.html')
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def check_submit():
 	if request.method == 'POST':
         # check if the post request has the file part
 		if 'file' not in request.files:
 			flash('No file part')
 			return redirect(request.url)
 		file = request.files['file']
+		#data_type = request.form.get('type', None)
 		if file.filename == '':
 			flash('No file selected for uploading')
 			return redirect(request.url)
@@ -29,14 +32,43 @@ def upload_file():
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			flash('File successfully uploaded')
-			data_type = request.form.get('type', None)
-			if data_type != None:
-				return redirect('/')
-			else:
-				flash('Please choose data type!')
+			return redirect('/')
 		else:
 			flash('Allowed file types are xlsx and xls')
 			return redirect(request.url)
+
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend():
+	file = request.files['file']
+	df = pd.read_excel(file.filename)
+	n = request.form['neighbors']
+	if request.form['type'] == 'indication':
+
+		from indication_model import get_indication_neighbors
+		
+		clean = df.loc[df.Indication.notnull()].copy()
+		unclean = df.loc[df.Indication.isnull()].copy()
+   		
+		neighborhood = get_indication_neighbors(clean.copy(), unclean.copy(), n)
+		distances = neighborhood[0]
+		neighbors = neighborhood[1]
+
+		clean.fillna('',inplace=True)
+		clean.reset_index(inplace=True)
+		clean.drop(columns='index', inplace=True)
+
+		label0 = list(clean.loc[neighbors.tolist()[0], 'Indication'])
+		sortedVotes = sorted(Counter(label0).items(), key=operator.itemgetter(1), reverse=True)
+		return render_template('success.html', indication=sortedVotes[0][0])
+
+	# elif request.form['type'] == 'drug':
+	# 	from drug_model import get_drug_neighbors
+
+	# 	clean = df.loc[df.Drug.notnull()].copy()
+	# 	unclean = df.loc[df.Drug.isnull()].copy()
+
+	# 	neighbors = get_drug_neighbors(clean, unclean, n)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
