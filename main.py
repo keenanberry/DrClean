737b7aa77d84b6ae72069f741520ae84bd01f987
Keenan Berry <keenanberry22@gmail.com>
2019-08-02 18:00:02 -0500

    fixed some bugs with initial app setup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, sys
 import urllib.request
 from app import app
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
 import pandas as pd
 import operator
@@ -24,7 +24,10 @@ def check_submit():
 			flash('No file part')
 			return redirect(request.url)
 		file = request.files['file']
-		#data_type = request.form.get('type', None)
+		n = request.form['number']
+		if n == '':
+			n = '5'
+		data = request.form['mySelect']
 		if file.filename == '':
 			flash('No file selected for uploading')
 			return redirect(request.url)
@@ -32,42 +35,48 @@ def check_submit():
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			flash('File successfully uploaded')
-			return redirect('/')
+			return redirect(url_for('model', filename=filename, data=data, n=n))
+			#return render_template('build.html')
 		else:
 			flash('Allowed file types are xlsx and xls')
 			return redirect(request.url)
 
-@app.route('/recommend', methods=['GET', 'POST'])
-def recommend():
-	file = request.files['file']
-	df = pd.read_excel(file.filename)
-	n = request.form['neighbors']
-	if request.form['type'] == 'indication':
+@app.route('/model/<filename>/<data>/<n>', methods=['GET', 'POST'])
+def model(filename, data, n):
+	if request.method == 'GET':
+		print('Building the model... this may take a minute.')
+		#file = request.files['file']
+		#filename = secure_filename(file.filename)
+		#df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+		#filename = secure_filename(file.filename)
+		df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#n = request.form['neighbors']
+		if data == 'indication':
 
-		from indication_model import get_indication_neighbors
-		
-		clean = df.loc[df.Indication.notnull()].copy()
-		unclean = df.loc[df.Indication.isnull()].copy()
-   		
-		neighborhood = get_indication_neighbors(clean.copy(), unclean.copy(), n)
-		distances = neighborhood[0]
-		neighbors = neighborhood[1]
+			from indication_model import get_indication_neighbors
+			
+			clean = df.loc[df.Indication.notnull()].copy()
+			unclean = df.loc[df.Indication.isnull()].copy()
 
-		clean.fillna('',inplace=True)
-		clean.reset_index(inplace=True)
-		clean.drop(columns='index', inplace=True)
+			neighborhood = get_indication_neighbors(clean.copy(), unclean.copy(), int(n))
+			distances = neighborhood[0]
+			neighbors = neighborhood[1]
 
-		label0 = list(clean.loc[neighbors.tolist()[0], 'Indication'])
-		sortedVotes = sorted(Counter(label0).items(), key=operator.itemgetter(1), reverse=True)
-		return render_template('success.html', indication=sortedVotes[0][0])
+			clean.fillna('',inplace=True)
+			clean.reset_index(inplace=True)
+			clean.drop(columns='index', inplace=True)
 
-	# elif request.form['type'] == 'drug':
-	# 	from drug_model import get_drug_neighbors
+			label0 = list(clean.loc[neighbors.tolist()[0], 'Indication'])
+			sortedVotes = sorted(Counter(label0).items(), key=operator.itemgetter(1), reverse=True)
+			return render_template('success.html', indication=sortedVotes[0][0])
 
-	# 	clean = df.loc[df.Drug.notnull()].copy()
-	# 	unclean = df.loc[df.Drug.isnull()].copy()
+		# elif request.form['type'] == 'drug':
+		# 	from drug_model import get_drug_neighbors
 
-	# 	neighbors = get_drug_neighbors(clean, unclean, n)
+		# 	clean = df.loc[df.Drug.notnull()].copy()
+		# 	unclean = df.loc[df.Drug.isnull()].copy()
+
+		# 	neighbors = get_drug_neighbors(clean, unclean, n)
 
 
 if __name__ == "__main__":
